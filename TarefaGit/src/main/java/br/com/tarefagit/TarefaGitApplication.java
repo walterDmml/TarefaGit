8470b39105b8cc5ca9ec5360e995f4c8c94dcd8d
Ruivo <Ruivo@pc.local>
2018-04-25 08:21:14 -0300

    Primeiro commit da aula de GIT.

---
+++ TarefaGit/src/main/java/br/com/tarefagit/TarefaGitApplication.java
@@ -0,0 +1,12 @@
+package br.com.tarefagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaGitApplication.class, args);
+	}
+}
---
+++ TarefaGit/src/main/java/br/com/tarefagit/walter.java
@@ -0,0 +1,7 @@
+package br.com.tarefagit;
+
+public class walter {
+		public static void main(String[] args) {
+			System.out.println("testando");
+		}	
+	}
